9aa80d39634d18c6805fb097d58434833f7aadb7
Mike De Wolfe <dewolfe.michael@gmail.com>
2023-12-02 21:01:59 -0500

    fix

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     println!("Day 1, Part 1; Trebuchet Result: {:?}", trebuchet_result);
 
     let cube_result: CubeResult = cube_conundrum::go();
-    println!("Day 1, Part 2; Cube Conundrum Result: {:?}", cube_result);
+    println!("Day 2, Cube Conundrum Result: {:?}", cube_result);
 }
